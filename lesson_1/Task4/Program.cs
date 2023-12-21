3f7f84f838ee9f6aec98f6aa6ea21642fb019f08
Artem <yukin.ar@yandex.ru>
2023-12-21 21:21:28 +0800

    add lesson1:Task4

---
+++ lesson_1/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 5;
+int b = 7;
+int c = 15;
+int d = 9;
+int e = 11;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
