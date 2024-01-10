1db083e7c63889eaebe4d6c9884604673148956b
Artem <yukin.ar@yandex.ru>
2024-01-10 19:57:52 +0800

    lesson2: add Task3

---
+++ lesson_2/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int n = 10;
+int[] array = { 3, 8, 46, 35, 23, 16, 7, 9, 12, 13 };
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
