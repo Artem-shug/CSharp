9d3fbc184861e114551b6f0525a20b2aa2512c62
Artem <yukin.ar@yandex.ru>
2024-01-13 21:40:46 +0800

    lesson3: add Task1

---
+++ lesson_3/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenet = a * b;
+    int denumenet = c + d;
+    double result = numenet / denumenet;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
