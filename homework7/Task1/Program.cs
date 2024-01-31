c158b2878e9e7f5a347d597316eac21762fbb396
Artem <yukin.ar@yandex.ru>
2024-01-31 19:05:56 +0800

    Homework 7

---
+++ homework7/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//  Даны два неотрицательных числа m и n.
+
+
+Console.Write("Введите неотрицательное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите неотрицательное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int ack = Ackkerman(n, m);
+Console.WriteLine(ack);
+
+int Ackkerman (int num1, int num2)
+{
+    if (num1 == 0) return num2 + 1;
+    else if (num2 == 0)
+    {
+        return Ackkerman(num1 - 1, 1);
+    }
+    else
+    {
+        return Ackkerman(num1 - 1, Ackkerman(num1, num2 - 1));
+    }
+}
---
+++ homework7/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+//Использовать рекурсию, не использовать циклы.
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+void ReverseArray(int[] array, int index) 
+{ 
+    if (index > array.Length - 1 || index < 0) 
+        return; 
+    ReverseArray(array, index + 1); 
+    Console.Write($"{array[index]} "); 
+}
+
+int[] arr = {1, 2, 5, 0, 10, 34};
+ReverseArray(arr, 0);
